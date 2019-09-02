c3dee8c4791ba01c3cd20417fd6ba2ea411ccaee
Younsoo, Shin <ys@betweak.com>
2019-09-02 13:53:15 +0900

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,13 @@
+curl -O https://hyperledger.github.io/composer/latest/prereqs-ubuntu.sh
+chmod u+x prereqs-ubuntu.sh
+./prereqs-ubuntu.sh
+npm install -g composer-cli@0.20
+npm install -g composer-rest-server@0.20
+npm install -g generator-hyperledger-composer@0.20
+npm install -g yo
+npm install -g composer-playground@0.20
+
+mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
+
+curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
+tar -xvf fabric-dev-servers.tar.gz
