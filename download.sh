07c002f7cc7861fb4d011e97c5ba6bdb7b466872
Younsoo, Shin <ys@betweak.com>
2019-09-02 14:00:01 +0900

    a

--- download.sh
+++ download.sh
@@ -10,4 +10,4 @@ npm install -g composer-playground@0.20
 mkdir ~/fabric-dev-servers && cd ~/fabric-dev-servers
 
 curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.tar.gz
-tar -xvf fabric-dev-servers.tar.gz
+tar -xvf ~/fabric-dev-servers.tar.gz
